2a211594e2c03b92d6489f4da0872952cfd570eb
vstar <v_star@naver.com>
2026-05-29 17:07:13 +0900

    Update AppTestApplication.java

--- app-test/src/main/java/com/example/app_test/AppTestApplication.java
+++ app-test/src/main/java/com/example/app_test/AppTestApplication.java
@@ -1,9 +1,11 @@
 package com.example.app_test;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class AppTestApplication {
 
 	public static void main(String[] args) {
